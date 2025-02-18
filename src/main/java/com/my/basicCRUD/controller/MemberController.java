cdb032a15f125330c110030874201594bcdddb86
seegeegaja <wnsdyd821@gmail.com>
2025-02-18 15:12:00 +0900

    '@ModelAttribute' 참말로 배울꺼 많아이잉

--- src/main/java/com/my/basicCRUD/controller/MemberController.java
+++ src/main/java/com/my/basicCRUD/controller/MemberController.java
@@ -3,16 +3,20 @@ package com.my.basicCRUD.controller;
 import com.my.basicCRUD.dto.MemberDto;
 import com.my.basicCRUD.entity.Member;
 import com.my.basicCRUD.service.MemberService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 @RequestMapping("/member")
+@Slf4j
 public class MemberController {
     private final MemberService memberService;
 
@@ -21,14 +25,27 @@ public class MemberController {
     }
 
     @GetMapping("/insertForm")
-    public String insertFormView() {
+    public String insertFormView( Model model) {
+        model.addAttribute("memberDto", new MemberDto());
         return "insertMember";
     }
 
     @PostMapping("/insert")
-    public String insert(MemberDto memberDto) {
-        System.out.println(memberDto);
-        return "showMember";
+    public String insert(
+            @Valid MemberDto memberDto,
+            BindingResult bindingResult,
+            RedirectAttributes redirectAttributes) {
+        //Validation Check 진행
+        if (bindingResult.hasErrors()) {
+            //오류 있을때
+            log.info("=============== validationError");
+            return "insertMember";
+        }
+        System.out.println("insert 에서 찍혀야한느것" + memberDto);
+        //받은 Dto를 서비스에 넘겨주고 저장요청한다
+        memberService.saveMember(memberDto);
+        redirectAttributes.addFlashAttribute("msg", "신규데이터가 입력되었습니다");
+        return "redirect:/member/view";
     }
 
     @GetMapping("view")
@@ -39,4 +56,34 @@ public class MemberController {
         return "showMember";
     }
 
+    @GetMapping("delete/{id}")
+    public String deleteId(@PathVariable("id") Long id, RedirectAttributes redirectAttributes) {
+
+        memberService.deleteById(id);
+        redirectAttributes.addFlashAttribute("msg", "선택한 자료가 삭제되었습니다");
+        return "redirect:/member/view";
+    }
+
+    @GetMapping("update/{id}")
+    public String updateFormView(@PathVariable("id") Long id, Model model) {
+        MemberDto dto = memberService.findByid(id);
+        log.info("===========dto : " + dto);
+        model.addAttribute("member", dto);
+        return "updateMember";
+    }
+
+    @PostMapping("update")
+    public String update(@Valid @ModelAttribute("member") MemberDto memberDto,
+                         BindingResult bindingResult ,
+                         RedirectAttributes redirectAttributes) {
+        if (bindingResult.hasErrors()) {
+            return "updateMember";
+        }
+        //수정요청
+        log.info("dto update : " + memberDto);
+        memberService.updateMember(memberDto);
+        //메시지 전송
+        redirectAttributes.addFlashAttribute("msg", "정상적으로 수정되었습니다");
+        return "redirect:/member/view";
+    }
 }
--- src/main/java/com/my/basicCRUD/service/MemberService.java
+++ src/main/java/com/my/basicCRUD/service/MemberService.java
@@ -5,6 +5,7 @@ import com.my.basicCRUD.entity.Member;
 import com.my.basicCRUD.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -28,5 +29,34 @@ public class MemberService {
 //            System.out.println("for로 서비스에서 옮겨 가져오는것"+dto);
 //            dtoList.add(dto);
 //        }
+    }
+    public void saveMember(MemberDto memberDto) {
+        //DTO -> Entity 로 변화
+        Member member = MemberDto.fromDto(memberDto);
+        // 정장요청
+        memberRepository.save(member);
+    }
+
+    public void deleteById(Long id) {
+        memberRepository.deleteById(id);
+    }
+
+    public MemberDto findByid(Long id) {
+        //id로 디비에서 검색하기
+        Member member = memberRepository.findById(id).orElse(null);
+        //DTO로 변환
+        if (!ObjectUtils.isEmpty(member)) {
+            return MemberDto.fromEntity(member);
+        } else {
+            return null;
+        }
+    }
+
+    public void updateMember(MemberDto dto) {
+        //dto로 받았으니까 Entity로 변환
+        Member member = MemberDto.fromDto(dto);
+        //수정처리
+        // save() : 해당ID가 존재 하면 수정 , 없으면 입력
+        memberRepository.save(member);
     }
 }
